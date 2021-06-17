9447a859017b2dd1c4523b8a658aa89aae5c3ce9
jean-creat <82119205+jean-creat@users.noreply.github.com>
2021-06-18 01:55:54 +1200

    Create style.js

---
+++ style.js
@@ -0,0 +1,7 @@
+.background { 
+  background: ("/Image/IMG_3726.JPG") no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
